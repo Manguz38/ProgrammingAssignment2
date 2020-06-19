142f8490fee0710758f0985f287766a604a07a2f
Manguz38 <mario.guzman@est.uexternado.edu.co>
2020-06-19 11:46:09 -0500

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -18,8 +18,8 @@ R session:
 > #
 > # A simple matrix m1 with a simple matrix inverse n1
 > # Define
-> m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
-> m1
+ m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+ m1
       [,1]  [,2]
 [1,]  0.50 -1.00
 [2,] -0.25  0.75
@@ -49,8 +49,8 @@ R session:
 > # And so (by linear algebra) n1 %*% m1 is also equal I2.
 > # (If n1 is the inverse of m1 then m1 is the inverse of n1.)
 > # With m1 defined as above, n1 ( the inverse of m1) is
-> n1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
-> n1
+ n1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
+ n1
      [,1] [,2]
 [1,]    6    8
 [2,]    2    4
@@ -118,3 +118,44 @@ getting cached data
 [1,]    3    7
 [2,]    1    5
 > 
+#### Creation of the function makeCacheMatrix and CacheSolve################
+
+makeCacheMatrix <-   function(x = numeric()){
+   m <- NULL
+   set <- function(y){
+      x <<- y
+      m <<- NULL
+   }
+   get <- function() x
+   setinverse <- function(solve) m <<- solve
+   getinverse <- function() m
+   list(set = set, get=get, 
+        setinverse = setinverse,
+        getinverse = getinverse)
+} 
+## In order to create the function we first had 
+#to create the function makecacheMatrix to achieve the following results:
+# set the value of the vector, get the value of the vector, set the value of the inverse and get the value of the inverse
+#in this example the m represents the future value of the matrix and and with the data
+#setinverse and getinverse we manage to calculate the inverse of the named matrix.
+
+
+cacheSolve <- function(x, ...){
+   m <- x$getinverse()
+   if(!is.null(m)) {
+      message("getting cache data")
+      return(m)
+   }
+   data <- x$get()
+   m <- solve(data,...)
+   x$setinverse(m)
+   m
+}
+### Afterwords with create the function cacheSolve to check if the inverse has
+# been created previously or it has to calculate it. We use solve() to obtain the 
+#represented inverse of the named matrix. 
+myMatrix_object <- makeCacheMatrix(m1)
+cacheSolve(myMatrix_object)
+# Using the above example we manage to 
+
+
